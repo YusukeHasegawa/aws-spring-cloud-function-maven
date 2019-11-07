960fa5c4d6fe7a15580e4082651967e91e30b6a5
YusukeHasegawa <hasegawa@inter-office.co.jp>
2019-11-07 14:40:24 +0900

    ライブラリのバージョンをアップ

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,16 @@
 
+import config.Config;
+import config.Properties;
+import functions.Function;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import config.Properties;
-
-@SpringBootApplication(scanBasePackages = "config")
+@SpringBootApplication(scanBasePackageClasses = {Function.class, Config.class})
 @EnableConfigurationProperties(Properties.class)
 public class Application {
 
-	public static void main(final String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,9 +1,7 @@
 package config;
 
-import org.springframework.cloud.function.context.FunctionScan;
 import org.springframework.context.annotation.Configuration;
 
-@FunctionScan
 @Configuration
 public class Config {
 
